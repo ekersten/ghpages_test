822831054fd0701bc866b0a19fce4a12d66c0c51
Eric Kersten <ekersten@gmail.com>
2020-05-09 10:49:38 -0300

    Updates

---
+++ precache-manifest.b850e3d1f6cca278e4baa69956033858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd491a7f9285c6ab2a8a56a62dd78d5e",
+    "url": "/ghpages_test/index.html"
+  },
+  {
+    "revision": "84ffed7bef3c8a80e1db",
+    "url": "/ghpages_test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a2f34cbc5cfbac1e948",
+    "url": "/ghpages_test/static/js/2.f40b571b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages_test/static/js/2.f40b571b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ffed7bef3c8a80e1db",
+    "url": "/ghpages_test/static/js/main.c68bc416.chunk.js"
+  },
+  {
+    "revision": "af8375c83f5b2396b467",
+    "url": "/ghpages_test/static/js/runtime-main.0ed08ec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
